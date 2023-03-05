05168428e5b7375b6c5e4bab6efb11307defa849
David Rowthorn <davidrowthorn@gmail.com>
2023-03-05 09:54:38 +0000

    nav bar working

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+const currentPanel = 'home'
+
 document.querySelector('#menu-icon').addEventListener('click', () => {
   const menu = document.querySelector('#menu')
   menu.style.height = menu.style.height === '0px' ? '180px' : '0px'
+})
+
+const panels = ['home', 'photo', 'video', 'play', 'contact']
+
+panels.forEach(clickedPanel => {
+  document
+    .querySelector(`#${clickedPanel}-button`)
+    .addEventListener('click', () => {
+      panels.forEach(p => {
+        document.querySelector(`#${p}-panel`).style.opacity =
+          p === clickedPanel ? 1 : 0
+      })
+    })
 })
