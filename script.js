d7050fd415aacb3246b523093f6d8d064b1cd79c
Simon Redhead <snr21@cam.ac.uk>
2024-06-04 22:44:48 +0100

    Link to Email Octopus landing page

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-document.getElementById('email').addEventListener('click', () => {
-    // Prevent crawlers from getting the email address.
-    const instruction = 'mailto'
-    const name = 'davidrowthorn'
-    const domain = 'gmail'
-    const suffix = 'com'
-
-    window.location.href = `${instruction}:${name}@${domain}.${suffix}`
-})
-
 class Venue {
     static Newnham = new Venue("Newnham Croft Social Club");
     static Tram = new Venue("The Tram Depot");
