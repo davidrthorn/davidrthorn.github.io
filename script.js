4c43bc11a3aeeeea2a4e9890dcb1ac212ffd3af5
Simon Redhead <snr21@cam.ac.uk>
2024-12-03 14:02:21 +0000

    Update email address

--- script.js
+++ script.js
@@ -86,8 +86,8 @@ if (next.msg !== undefined) {
 document.getElementById('email').addEventListener('click', () => {
   // Prevent crawlers from getting the email address.
   const instruction = 'mailto'
-  const name = 'davidrowthorn'
-  const domain = 'gmail'
+  const name = 'cambridgejazzclub'
+  const domain = 'meeprophone'
   const suffix = 'com'
 
   window.location.href = `${instruction}:${name}@${domain}.${suffix}`
